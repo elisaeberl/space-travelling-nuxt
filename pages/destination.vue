dfbe8bc1aca301bdfba496292af5424243c69108
Elisa <elisajohanna123@gmail.com>
2022-03-04 10:17:35 +0100

    Update destination.vue

--- pages/destination.vue
+++ pages/destination.vue
@@ -1,3 +1,25 @@
 <template>
   <h1>Destination</h1>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      destinations: [],
+    }
+  },
+  methods: {
+    async loadData() {
+      const response = await fetch(
+        'https://space.wp.seiwald.hak-stjo.schulwebspace.at/wp-json/wp/v2/destination'
+      )
+      this.destination = await response.json()
+  },
+  },
+  mounted() {
+    this.loadData()
+    document.body.style.backgroundImage = "url('background-destination-mobile.jpg')"
+  },
+}
+</script>
